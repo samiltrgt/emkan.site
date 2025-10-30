4f01419fcd20504481de3f40ae7f665633d4ddd6
Şamil Turgut <samiltrgt07@gmail.com>
2025-10-31 02:17:51 +0300

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -53,7 +53,7 @@ export default function Hero({ title, subtitle, ctaPrimary, ctaSecondary }: Hero
           <motion.img
             src="/logos/emkanbeyaz2.png"
             alt="Emkan Global"
-            className="mx-auto h-auto w-[200px] md:w-[300px] lg:w-[460px]"
+            className="mx-auto h-auto w-[200px] md:w-[300px] lg:w-[435px]"
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1.2, delay: 0.2, ease: "easeOut" }}
